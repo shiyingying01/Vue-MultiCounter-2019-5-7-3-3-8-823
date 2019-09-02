bd35427e0fe62fbe899ee07b9657033dc48e46ff
shiyingying01 <718275303@qq.com>
2019-09-02 20:06:42 +0800

    multiCounter

---
+++ multicounter/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <br />
+    <input v-model="countNum" />
+    <Counter v-for="n in getNum()"
+     v-bind:key="n" 
+     v-bind:index="n" 
+     v-on:getSum="count"
+     />
+    {{sum}}
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      countNum: 0,
+      sum:0
+    };
+  },
+  methods: {
+    getNum: function() {
+      if (this.isNumber(this.countNum)) {
+        return parseInt(this.countNum);
+      } else {
+        return 0;
+      }
+    },
+
+    isNumber: function(n) {
+      return !isNaN(parseFloat(n) && isFinite(n));
+    },
+
+    count: function(e) {
+      this.sum = this.sum + e;
+    }
+  },
+  components: {
+    Counter
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ multicounter/src/components/Counter.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="counter">
+    <h1>{{msg}}</h1>
+    <span>{{index}}:</span>&emsp;
+    <button @click="add">+</button>&emsp;
+    <span>{{count}}</span>&emsp;
+    <button @click="sub">-</button>;
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  props: {
+    msg: String,
+    index: Number
+  },
+
+  data() {
+    return {
+      count: this.index
+    };
+  },
+  methods: {
+    //增加
+    add() {
+      this.count++;
+      this.$emit("getSum", 1);
+    },
+    //减少
+    sub() {
+      this.count--;
+      this.$emit("getSum", -1);
+    }
+  },
+  created() {
+    this.$emit("getSum", this.index);
+  },
+
+  destroyed() {
+    this.$emit("getSum", -this.count);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
